f0b6038f1f97e43d716dabb5158a3a1582fb75d5
max <mdd.bsd@gmail.com>
2025-12-10 20:10:12 -0300

    el hero ya me gusta como quedo

--- src/componentes/landing.tsx
+++ src/componentes/landing.tsx
@@ -3,7 +3,7 @@ import HeroInicio from "./layout/heros/heroinicio"
 
 export default function Landing(){
     return(
-        <div id="envolturalanding" className="">
+        <div id="envolturalanding" className="bg-fondocfl">
             <div id="hero">
                 <HeroInicio />
             </div>
--- src/componentes/layout/heros/heroinicio.tsx
+++ src/componentes/layout/heros/heroinicio.tsx
@@ -6,16 +6,16 @@ import imgHero from '../../../img/heroinicio-placeholder.jpeg'
 
 export default function HeroInicio(){
     return(
-        <div id="hero" className='flex max-[800px]:flex-col justify-center bg-blue-500 p-10 gap-4 py-20 px-15'>
+        <div id="hero" className='flex max-[800px]:flex-col justify-center bg-azulcfl gap-4 p-15 max-[800px]:p-3'>
             <div id="herotexto" className='text-white max-[800px]:order-last'>
                 <h1 className='text-5xl font-extrabold'>Transformá tu futuro con educación de calidad</h1>
                 <p className='py-10 text-lg'>En CFL 414 La Matanza ofrecemos formación profesional gratuita con certificación oficial para potenciar tu desarrollo personal y laboral.</p>
-                <div id="herobotones" className='flex gap-10  text-blue-500 [&_#boton]:bg-white [&_#boton]:rounded-md [&_#boton]:p-5 [&_#boton]:hover:bg-orange-500 [&_#boton]:hover:text-white'>
+                <div id="herobotones" className='flex max-[800px]:flex-col gap-10  font-bold text-blue-500 [&_#boton]:bg-white [&_#boton]:rounded-md [&_#boton]:p-5 [&_#boton]:hover:bg-orange-500 [&_#boton]:hover:text-white [&_#boton]:shrink-0'>
                     <Link id="boton" to="/">Ver cursos disponibles</Link>
                     <Link id="boton" to="/">Más información</Link>
                 </div>
             </div>
-            <div id="heroimagen" className='size-100 max-[800px]:w-full max-[800px]:order-first'>
+            <div id="heroimagen" className='max-[800px]:w-full max-[800px]:order-first'>
                 <img src={imgHero} className=''/>
             </div>
         </div> 
